429478507e7b292c0ab1eb564589dff374f6cead
thismarvin <bend_@outlook.com>
2020-06-30 13:31:51 -0500

    first pass at command aliases

--- src/obelus/command.js
+++ src/obelus/command.js
@@ -6,8 +6,23 @@ class Command {
 
 	update(message, args) {
 		for (let i = 0; i < this.args.length; i++) {
-			if (this.args[i] !== args[i]) {
-				return;
+			if (this.args[i].indexOf("|") !== -1) {
+				const aliases = this.args[i].split("|");
+				let valid = false;
+				for (let alias of aliases) {
+					if (alias === args[i]) {
+						valid = true;
+						break;
+					}
+				}
+
+				if (!valid) {
+					return;
+				}
+			} else {
+				if (this.args[i] !== args[i]) {
+					return;
+				}
 			}
 		}
 
